b6f6b22dacf7e25ab72420f829b0450e27d854f4
KanetTran-PC\Kanet Tran <k4netmt@gmail.com>
2016-09-28 22:31:38 +0700

    1. Made to the echo request 2. Made to the proxy request. 3. Apply CLI. 4. Apply stdout to write and save log.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var fs=require('fs')
 var request = require('request');
 var argv=require('yargs').argv
+
+var logStream=argv.logfile ? fs.createWriteStream(argv.logfile) : process.stdout
 
 var localhost='127.0.0.1'
 var scheme='http://'
@@ -8,24 +11,31 @@ var host=argv.host|| '127.0.0.1'
 var port=argv.port|| (host===localhost ?  8000: 80)
 var destinaltionUrl=scheme + host + ':' + port
 
-
 var echoServer = http.createServer(function (req, res) {
-    console.log(req.headers)
+    logStream.write('echoServer')
     for (var header in req.headers) {
         res.setHeader(header, req.headers[header])
     }
+    logStream.write(JSON.stringify(req.headers)+"\n")
     req.pipe(res)
     // res.end(res.toString())
 })
 echoServer.listen(8000)
+logStream.write('proxyServer listening @ 127.0.0.1:8000 \n')
 
 var proxyServer = http.createServer(function (req, res) {
-    console.log("proxyserver")
-    var options = {
-        url: destinaltionUrl + req.url
+    logStream.write("proxyserver")
+    logStream.write(JSON.stringify(req.headers)+"\n")
+    //x-destination-url
+    var url= destinaltionUrl
+    if (req.headers['x-destination-url']){
+        url='http://'+req.headers['x-destination-url']
     }
-    var outboundRequest=request(options)
-    req.pipe(outboundRequest).pipe(res)
+    var options = {
+        url: url + req.url
+    }
+    req.pipe(request(options)).pipe(res)
     // res.end(res.toString())
 })
 proxyServer.listen(9000)
+logStream.write('proxyServer listening @ 127.0.0.1:9000 \n')
